c81809be7bc412700dbe166223b39dc6a39d8ef8
Austin Bachurski <austinbachurski@gmail.com>
2026-01-07 14:09:22 -0700

    Fix demo unused errors.

--- src/memoryDemo.cpp
+++ src/memoryDemo.cpp
@@ -1,19 +1,22 @@
-#include "memoryAllocator.hpp"
 #include "memoryManager.hpp"
 #include "job.hpp"
 
 #include <cstdint>
 #include <functional>
+#include <print>
 #include <span>
 
 
 void demo(std::function<std::size_t(std::span<Job const, 20>, uint8_t const)> allocator)
 {
-    MemoryManager memory(Allocator::firstFit);
+    MemoryManager memory(allocator);
 
     Job page{1, 2, 1, 7, JobState::running, JobState::sleeping};
 
-    memory.allocate(page);
+    if (memory.allocate(page) == MemoryManager::AllocationResult::failure)
+    {
+        std::println("allocation failed!");
+    }
 
     uint8_t currentTime = 0u;
 
