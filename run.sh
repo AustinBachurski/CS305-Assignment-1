e8bccf0c6b051656385af7018ff73b63e5267c43
Austin Bachurski <austinbachurski@gmail.com>
2026-01-08 13:44:40 -0700

    Make run script start to finish.

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
+# Create build directory and configure cmake if `build` does not exist.
+if [[ ! -d build ]]; then
+    cmake -B build
+fi
+
+# Build project.
 cmake --build build
+
+# Run project for each algorithm, outputting to file.
 ./build/bin/assignment1 --First-Fit > output/First-Fit.txt
 ./build/bin/assignment1 --Best-Fit > output/Best-Fit.txt
 ./build/bin/assignment1 --Worst-Fit > output/Worst-Fit.txt
+
+# Status code, info dump, and exit.
 echo "Program exited with code $?.  Output written to 'output' directory."
 
