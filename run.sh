5184393f004cd187630763bb56c12e9f9c48c45d
Austin Bachurski <austinbachurski@gmail.com>
2026-01-08 13:18:17 -0700

    Set algorithm via argv, output to file.

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 cmake --build build
-./build/bin/assignment1
+./build/bin/assignment1 --First-Fit > output/First-Fit.txt
+./build/bin/assignment1 --Best-Fit > output/Best-Fit.txt
+./build/bin/assignment1 --Worst-Fit > output/Worst-Fit.txt
+echo "Program exited with code $?.  Output written to 'output' directory."
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,32 @@
 #include "job.hpp"
 
 #include <cstdint>
+#include <cstring>
 #include <functional>
+#include <print>
 #include <span>
+
 
 void demo(std::function<std::size_t(std::span<Job const, 20>, uint8_t const)> allocator);
 
 
-int main()
+int main(int argc, char **argv)
 {
-    //demo(Allocator::firstFit);
-    //demo(Allocator::bestFit);
-    demo(Allocator::worstFit);
+    if (argc < 2)
+    {
+        std::println("Usage: assignment1 <--algorithm> (i.e. assignment1 --First-Fit)\nAvailable algorithms:\n\t\tFirst-Fit\n\t\tBest-Fit\n\t\tWorst-Fit");
+    }
+    else if (std::strcmp(argv[1], "--First-Fit") == 0)
+    {
+        demo(Allocator::firstFit);
+    }
+    else if (std::strcmp(argv[1], "--Best-Fit") == 0)
+    {
+        demo(Allocator::bestFit);
+    }
+    else if (std::strcmp(argv[1], "--Worst-Fit") == 0)
+    {
+        demo(Allocator::worstFit);
+    }
 }
 
